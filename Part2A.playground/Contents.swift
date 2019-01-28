96c38a778d09bf07293c9ba100db48847ade85b4
Cara Weggler <cweggler@gmail.com>
2019-01-28 08:49:25 -0600

    Removed Hello string and added comments

--- Part2A.playground/Contents.swift
+++ Part2A.playground/Contents.swift
@@ -1,7 +1,5 @@
 import UIKit
 
-var str = "Hello, playground"
-
 let regBusFare: Float = 2.50  // the cost to ride the bus during non-rush hours
 let rushBusFare: Float = 3.25 // the cost to ride the bus during rush hours
 let regRides: Float = 4       // the number of rides during non-rush hours
@@ -11,6 +9,7 @@ var totalRegCost:Float = regBusFare * regRides      // the total spent on regula
 var totalRushCost:Float = rushBusFare * rushRides   // the total spent on rush hour bus rides
 var totalCost: Float = totalRegCost + totalRushCost // the total spent on all bus rides
 
+// Print the total spent on regular and rush hour rides and all bus rides
 print("The total spent on regular bus rides is $\(totalRegCost)")
 print("The total spent on rush hour bus rides is $\(totalRushCost)")
 print("The total spent on all bus rides is $\(totalCost)")
